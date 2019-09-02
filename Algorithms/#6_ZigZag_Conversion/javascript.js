691507652cbcc05c1464dbf8379c62412411212d
Song Cho <song@netrue.co.kr>
2019-09-02 13:59:48 +0900

    problem statement

---
+++ Algorithms/#6_ZigZag_Conversion/javascript.js
@@ -0,0 +1,45 @@
+// 6. ZigZag Conversion
+// Related Topics: String
+// Solution Link: https://leetcode.com/problems/zigzag-conversion/solution/
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+// string convert(string s, int numRows);
+
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
+
+
+
+// by anthonyw
+// var length = s.length;
+// if (numRows == 1) return s;
+// var cycleLen = numRows * 2 - 2;
+// var aZig = [];
+// for (var i = 0; i < numRows; i++){
+//     for (var j = 0; j + i < length; j=j+cycleLen){
+//         aZig.push(s.charAt(j + i));
+//         if (i!=0 && i!=numRows -1 && j + cycleLen - i < length){
+//             aZig.push(s.charAt(j + cycleLen - i));
+//         }
+//     }
+// }
+
+// return aZig.join("");
